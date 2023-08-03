a94f18b0d0aced13582399635a35a1a2d67c6c4d
knhn1004 <kunhan945@gmail.com>
2023-08-03 16:53:38 -0700

    refactor bot package

--- main.go
+++ main.go
@@ -11,7 +11,6 @@ import (
 	"net/url"
 	"os"
 	"strings"
-	"time"
 
 	"github.com/cohere-ai/cohere-go"
 	"github.com/google/uuid"
@@ -19,8 +18,8 @@ import (
 	"github.com/minoplhy/duckduckgo-images-api"
 	supa "github.com/nedpals/supabase-go"
 	openai "github.com/sashabaranov/go-openai"
-	"github.com/shomali11/slacker/v2"
-	"github.com/slack-go/slack"
+
+	"github.com/knhn1004/WhatDoIEat/internal/bot"
 )
 
 var (
@@ -85,9 +84,7 @@ func (e *ErrorType) Error() string {
 func main() {
 	loadEnv()
 
-	bot := slacker.NewClient(botToken, appToken,
-		slacker.WithBotMode(slacker.BotModeIgnoreApp),
-	)
+	bot.InitializeBot(botToken, appToken)
 
 	supabase := supa.CreateClient(supabaseUrl, supabaseKey)
 	fmt.Printf("supabase: %v\n", supabase) // TODO: remove this
@@ -158,68 +155,10 @@ func main() {
 		}
 	}
 
-	bot.AddCommand(&slacker.CommandDefinition{
-		Command: "ping",
-		Handler: func(ctx *slacker.CommandContext) {
-			t1, _ := ctx.Response().Reply("about to be replaced 🚧️")
-			fmt.Println(ctx.Event().UserID)
-			fmt.Println(ctx.Event().UserProfile)
-
-			time.Sleep(time.Second)
-
-			ctx.Response().Reply("pong🏓️", slacker.WithReplace(t1))
-		},
-	})
-	bot.AddCommand(&slacker.CommandDefinition{
-		Command:     "upload <sentence>",
-		Description: "Upload a sentence!",
-		Handler: func(ctx *slacker.CommandContext) {
-			sentence := ctx.Request().Param("sentence")
-			api := ctx.SlackClient()
-			event := ctx.Event()
-
-			api.PostMessage(event.ChannelID, slack.MsgOptionText("📄️ Uploading file ...", false))
-			_, err := api.UploadFile(slack.FileUploadParameters{Content: sentence, Channels: []string{event.ChannelID}})
-			if err != nil {
-				ctx.Response().ReplyError(err)
-			}
-		},
-	})
-	bot.AddCommand(&slacker.CommandDefinition{
-		Command:     "echo {word}",
-		Description: "Echo a word!",
-		Handler: func(ctx *slacker.CommandContext) {
-			word := ctx.Request().Param("word")
-
-			attachments := []slack.Attachment{}
-			attachments = append(attachments, slack.Attachment{
-				Color:      "good",
-				AuthorName: "👨️ Raed Shomali",
-				Title:      "Attachment Title",
-				Text:       "Attachment Text",
-			})
-
-			ctx.Response().Reply(word, slacker.WithAttachments(attachments))
-		},
-	})
-
-	HelpDef := &slacker.CommandDefinition{
-		Command:     "help",
-		Description: "help!",
-		Handler: func(ctx *slacker.CommandContext) {
-			ctx.Response().Reply("Your own help function...")
-		},
-	}
-
-	bot.Help(HelpDef)
-
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
 
-	err = bot.Listen(ctx)
-	if err != nil {
-		log.Fatal(err)
-	}
+	bot.Start(ctx)
 }
 
 func loadEnv() error {
