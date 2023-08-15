e997cb2cc202b08e999b695295913c2f8e3af3ff
oliver <oliver@olivers-MacBook-Pro.local>
2023-08-15 08:53:56 -0700

    survey page w htmx

--- internal/web/controllers/survey_controller.go
+++ internal/web/controllers/survey_controller.go
@@ -1,11 +1,18 @@
-// internal/web/controllers/survey_controller.go
 package controllers
 
 import "github.com/gin-gonic/gin"
 
 func SurveyHandler(c *gin.Context) {
+	preferences := map[string]bool{
+		"Spicy Food":       true,
+		"Vegetarian Meals": false,
+		"Dairy-Free":       false,
+		"Seafood":          true,
+		"Gluten-Free":      false,
+	}
+
 	c.HTML(200, "survey.html", gin.H{
-		"Title":   "Survey Page",
-		"Content": "Some content you want to render...",
+		"Title":       "Survey Page",
+		"Preferences": preferences,
 	})
 }
